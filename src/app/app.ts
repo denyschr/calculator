56b75c9259c21b365d89480bd5ceaf2bb976cb68
denyschr <denprogman99@gmail.com>
2025-07-16 09:46:34 +0300

    feat: project setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'qa-root',
   imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected readonly title = signal('quick-apps');
