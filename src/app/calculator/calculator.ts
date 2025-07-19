c01d1aca86b3a8dbe46709e75382031c97356740
denyschr <denprogman99@gmail.com>
2025-07-19 16:27:34 +0300

    feat(calculator): handle operators, show expression

--- src/app/calculator/calculator.ts
+++ src/app/calculator/calculator.ts
@@ -1,28 +1,19 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  DoCheck,
+  inject,
+  signal
+} from '@angular/core';
 
 import { ThemeSwitcher } from './theme-switcher/theme-switcher';
 import { ThemeStore } from './theme-switcher/theme-store';
 
-type State = (typeof STATES)[keyof typeof STATES];
-type InputEvent = (typeof INPUT_EVENTS)[keyof typeof INPUT_EVENTS];
+type State = 'INITIAL' | 'LEFT_OPERAND' | 'OPERATOR' | 'RIGHT_OPERAND' | 'EQUALS';
+type InputEvent = 'ZERO' | 'DIGIT' | 'DOT' | 'OPERATOR' | 'EQUALS';
 type Digit = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
-type Operator = '+' | '-' | '*' | '/';
-
-const STATES = {
-  INITIAL: 0,
-  LEFT_OPERAND: 1,
-  OPERATOR: 2,
-  RIGHT_OPERAND: 3,
-  EQUALS: 4
-} as const;
-
-const INPUT_EVENTS = {
-  ZERO: 0,
-  DIGIT: 1,
-  DOT: 2,
-  OPERATOR: 3,
-  EQUALS: 4
-} as const;
+type Operator = '+' | '–' | '×' | '÷';
 
 @Component({
   selector: 'qa-calculator',
@@ -31,46 +22,78 @@ const INPUT_EVENTS = {
   imports: [ThemeSwitcher],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Calculator {
+export class Calculator implements DoCheck {
   protected readonly themeStore = inject(ThemeStore);
 
-  private currentState: State = STATES.INITIAL;
+  private currentState: State = 'INITIAL';
 
   protected readonly leftOperand = signal(0);
   protected readonly operator = signal<Operator | null>(null);
   protected readonly rightOperand = signal(0);
   protected readonly display = signal('0');
 
+  protected readonly expression = computed(() => {
+    const left = this.leftOperand();
+    const operator = this.operator();
+    const right = this.rightOperand();
+
+    switch (this.currentState) {
+      case 'OPERATOR':
+        return `${left} ${operator}`;
+      case 'EQUALS':
+        return `${left} ${operator} ${right} =`;
+      default:
+        return '';
+    }
+  });
+
+  public ngDoCheck(): void {
+    console.log(`currentState: ${this.currentState}`);
+    console.log(`leftOperand: ${this.leftOperand()}`);
+    console.log(`operator: ${this.operator()}`);
+    console.log(`rightOperand: ${this.rightOperand()}`);
+    console.log(`display: ${this.display()}`);
+  }
+
   protected enterDigit(digit: Digit): void {
     switch (this.currentState) {
-      case STATES.INITIAL:
-      case STATES.OPERATOR:
-      case STATES.EQUALS:
+      case 'INITIAL':
+      case 'OPERATOR':
+      case 'EQUALS':
         this.display.set(digit);
         break;
-      case STATES.LEFT_OPERAND:
-      case STATES.RIGHT_OPERAND:
+      case 'LEFT_OPERAND':
+      case 'RIGHT_OPERAND':
         this.display.update((value) => value + digit);
         break;
     }
-    this.updateState(INPUT_EVENTS.DIGIT);
+    this.updateState('DIGIT');
   }
 
   protected enterZero(): void {
     switch (this.currentState) {
-      case STATES.LEFT_OPERAND:
-      case STATES.RIGHT_OPERAND:
+      case 'LEFT_OPERAND':
+      case 'RIGHT_OPERAND':
         this.display.update((value) => value + '0');
         break;
-      case STATES.OPERATOR:
-      case STATES.EQUALS:
+      case 'OPERATOR':
+      case 'EQUALS':
         this.display.set('0');
         break;
     }
   }
 
   protected enterOperator(operator: Operator): void {
-    // ...
+    switch (this.currentState) {
+      case 'LEFT_OPERAND':
+        this.leftOperand.set(Number(this.display()));
+        this.operator.set(operator);
+        break;
+      case 'OPERATOR':
+        this.operator.set(operator);
+        break;
+    }
+    this.updateState('OPERATOR');
   }
 
   protected enterEquals(): void {
@@ -86,7 +109,7 @@ export class Calculator {
   }
 
   protected clear(): void {
-    this.currentState = STATES.INITIAL;
+    this.currentState = 'INITIAL';
     this.leftOperand.set(0);
     this.operator.set(null);
     this.rightOperand.set(0);
@@ -95,18 +118,24 @@ export class Calculator {
 
   private updateState(inputEvent: InputEvent): void {
     switch (this.currentState) {
-      case STATES.INITIAL:
-        if (inputEvent === INPUT_EVENTS.DIGIT) {
-          this.currentState = STATES.LEFT_OPERAND;
+      case 'INITIAL':
+        if (inputEvent === 'DIGIT') {
+          this.currentState = 'LEFT_OPERAND';
         }
         break;
-      case STATES.LEFT_OPERAND:
+      case 'LEFT_OPERAND':
+        if (inputEvent === 'OPERATOR') {
+          this.currentState = 'OPERATOR';
+        }
         break;
-      case STATES.OPERATOR:
+      case 'OPERATOR':
+        if (inputEvent === 'DIGIT') {
+          this.currentState = 'RIGHT_OPERAND';
+        }
         break;
-      case STATES.RIGHT_OPERAND:
+      case 'RIGHT_OPERAND':
         break;
-      case STATES.EQUALS:
+      case 'EQUALS':
         break;
     }
   }
