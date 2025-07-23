d3e7101c55b068a3ca9d231227b607c8de304204
denyschr <denprogman99@gmail.com>
2025-07-23 20:10:24 +0300

    feat(calculator): add keyboard input

--- src/app/calculator/calculator-keypad/calculator-keypad.ts
+++ src/app/calculator/calculator-keypad/calculator-keypad.ts
@@ -6,15 +6,68 @@ import { Digit, Operator } from '../types';
   selector: 'qa-calculator-keypad',
   templateUrl: './calculator-keypad.html',
   styleUrl: './calculator-keypad.css',
+  host: {
+    '(document:keydown)': 'onKeyDown($event)'
+  },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CalculatorKeypad {
   public readonly digitEntered = output<Digit>();
   public readonly operatorEntered = output<Operator>();
   public readonly dotEntered = output();
+  public readonly equalsEntered = output();
   public readonly percentEntered = output();
   public readonly negativeSignEntered = output();
-  public readonly equalsEntered = output();
   public readonly cleared = output();
   public readonly deleted = output();
+
+  protected onKeyDown(event: KeyboardEvent): void {
+    if ((event.target as HTMLElement)?.tabIndex >= 0) {
+      return;
+    }
+
+    const key = event.key;
+    if (/^[0-9]$/.test(key)) {
+      event.preventDefault();
+      return void this.digitEntered.emit(key as Digit);
+    }
+
+    switch (key) {
+      case '+':
+        this.operatorEntered.emit('+');
+        break;
+      case '-':
+        this.operatorEntered.emit('–');
+        break;
+      case '*':
+        this.operatorEntered.emit('×');
+        break;
+      case '/':
+        this.operatorEntered.emit('÷');
+        break;
+      case '.':
+        this.dotEntered.emit();
+        break;
+      case '=':
+      case 'Enter':
+        this.equalsEntered.emit();
+        break;
+      case '%':
+        this.percentEntered.emit();
+        break;
+      case 'F9':
+        this.negativeSignEntered.emit();
+        break;
+      case 'Escape':
+        this.cleared.emit();
+        break;
+      case 'Backspace':
+        this.deleted.emit();
+        break;
+      default:
+        return;
+    }
+
+    event.preventDefault();
+  }
 }
